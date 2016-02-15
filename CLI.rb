854568b4615d01193dfe150da3f3d69105837a32
HarrisonLavin <harrison.lavin@gmail.com>
2016-02-15 12:21:24 -0500

    Added gems to master??

--- CLI.rb
+++ CLI.rb
@@ -1,1 +1,8 @@
+#Provices command line interface for users
+input = 0
 
+while input != exit
+  puts "What genre would you like to act on?"
+  resource = gets.chomp
+  puts "What action would you like to take?"
+  action = gets.chomp
