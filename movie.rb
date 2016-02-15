d0cf64d51d68364302f416eb40f552c652897cf9
smallfryy <holly.m.peck@gmail.com>
2016-02-15 13:37:24 -0500

    test

--- movie.rb
+++ movie.rb
@@ -1,1 +1,17 @@
+# class definition: Movie
+# ======================
+# This is the class definition movie
+# for our program.
 
+class Movie
+
+  attr_accessor :title, :director, :actors, :genre, :year
+
+  def initialize(title)
+    @name = name
+
+  end
+
+
+
+end
